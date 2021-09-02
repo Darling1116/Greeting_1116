525c23a340082f48d867e53675d45844e84299ed
Darling1116 <1154883599@qq.com>
2021-09-02 22:40:19 +0800

    提交代码

---
+++ numpy&panads_1/test4.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#data = pd.Series(np.random.randn(1000), index=np.arange(1000))
+#data = data.cumsum()
+
+data = pd.DataFrame(np.random.randn(1000, 4),
+                    index=np.arange(1000),
+                    columns=list("ABCD"))
+data = data.cumsum()
+#print(data.head())
+# print(data)
+
+ax = data.plot.scatter(x='A', y='B', color='DarkBlue', label='Class 1')
+data.plot.scatter(x='A', y='C', color='DarkGreen', label='Class 2', ax=ax)
+
+plt.show()
