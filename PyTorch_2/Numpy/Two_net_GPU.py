ec2972e8c5c72b1b27e6b720005c824c58b1364a
Darling1116 <1154883599@qq.com>
2022-01-16 23:20:35 +0800

    提交代码

---
+++ PyTorch_2/Numpy/Two_net_GPU.py
@@ -0,0 +1,39 @@
+import torch
+
+dtype = torch.float  # dtype???
+device = torch.device("cpu")
+# device = torch.device（“cuda：0”）＃取消注释以在GPU上运行
+
+
+N, D_in, H, D_out = 64, 1000, 100, 10
+
+# 创建随机输入和输出数据
+x = torch.randn(N, D_in, device=device, dtype=dtype)
+y = torch.randn(N, D_out, device=device, dtype=dtype)
+
+# 随机初始化权重
+w1 = torch.randn(D_in, H, device=device, dtype=dtype)
+w2 = torch.randn(H, D_out, device=device, dtype=dtype)
+
+learning_rate = 1e-6
+for t in range(500):
+    # 前向传递：计算预测y
+    h = x.mm(w1)  # mm???
+    h_relu = h.clamp(min=0)  #clamp???
+    y_pred = h_relu.mm(w2)
+
+    # 计算和打印损失
+    loss = (y_pred - y).pow(2).sum().item()
+    print(t, loss)
+
+    # Backprop计算w1和w2相对于损耗loss的梯度-----?????
+    grad_y_pred = 2.0 * (y_pred - y)
+    grad_w2 = h_relu.t().mm(grad_y_pred)
+    grad_h_relu = grad_y_pred.mm(w2.t())
+    grad_h = grad_h_relu.clone()
+    grad_h[h < 0] = 0
+    grad_w1 = x.t().mm(grad_h)
+
+    # 使用梯度下降更新权重
+    w1 -= learning_rate * grad_w1
+    w2 -= learning_rate * grad_w2
