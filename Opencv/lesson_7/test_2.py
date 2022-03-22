402a9678f0e682da355d8f987577daa0fcefb78a
Darling1116 <1154883599@qq.com>
2022-03-22 23:41:21 +0800

    提交代码

--- Opencv/lesson_7/test_2.py
+++ Opencv/lesson_7/test_2.py
@@ -3,20 +3,12 @@ import cv2 as cv
 from matplotlib import pyplot as plt
 
 
-img = cv.imread('D:/PycharmProjects/pythonProject1/Opencv 4.5/images/gray1.jpg', 0)
+#---绘制直方图：使用matplotlib的法线图，彩色图像---
+img = cv.imread('D:/PycharmProjects/pythonProject1/Opencv 4.5/images/1.jpg')
+color = ('b','g','r')
 
-# create a mask
-mask = np.zeros(img.shape[:2], np.uint8)
-mask[100:300, 100:400] = 255
-masked_img = cv.bitwise_and(img,img,mask = mask)
-
-# 计算掩码区域和非掩码区域的直方图
-# 检查作为掩码的第三个参数
-hist_full = cv.calcHist([img],[0],None,[256],[0,256])
-hist_mask = cv.calcHist([img],[0],mask,[256],[0,256])
-plt.subplot(221), plt.imshow(img, 'gray')
-plt.subplot(222), plt.imshow(mask,'gray')
-plt.subplot(223), plt.imshow(masked_img, 'gray')
-plt.subplot(224), plt.plot(hist_full), plt.plot(hist_mask)
-plt.xlim([0,256])
+for i,col in enumerate(color):
+    histr = cv.calcHist([img],[i],None,[256],[0,256])
+    plt.plot(histr,color = col)
+    plt.xlim([0,256])
 plt.show()
